9dd435a08badb094ccf03cfd185075408499aa5d
MoccaDesignDev <moccadesignco@gmail.com>
2026-05-01 16:31:31 +0800

    Add: mergeSort - submission-0

---
+++ Data Structures & Algorithms/mergeSort/submission-0.py
@@ -0,0 +1,30 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def mergeSort(self, pairs: List[Pair]) -> List[Pair]:
+        if len(pairs) <= 1:
+            return pairs
+        
+        mid = len(pairs) // 2
+        left = self.mergeSort(pairs[:mid])
+        right = self.mergeSort(pairs[mid:])
+        
+        return self.merge(left, right)
+
+    def merge(self, left: List[Pair], right: List[Pair]) -> List[Pair]:
+        res = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i].key <= right[j].key:
+                res.append(left[i])
+                i += 1
+            else:
+                res.append(right[j])
+                j += 1
+        res.extend(left[i:])
+        res.extend(right[j:])
+        return res
+         
