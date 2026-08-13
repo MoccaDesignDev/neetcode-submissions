f6feb8903fe74039eb109e0390724146ca96e9f2
Walter Chang <moccadesignco@gmail.com>
2026-08-13 11:46:00 +0800

    Add: python-diamond-problem-and-method-resolution-order - submission-0

---
+++ Python OOP/python-diamond-problem-and-method-resolution-order/submission-0.py
@@ -0,0 +1,20 @@
+class A:
+    def print_method(self) -> None:
+        print("A")
+
+class B(A):
+    def print_method(self) -> None:
+        print("B")
+
+class C(A):
+    def print_method(self) -> None:
+        print("C")
+
+class D(C, B): 
+    def print_method(self) -> None:
+        print("B")
+
+
+# Do not change the code below
+d = D()
+d.print_method()
